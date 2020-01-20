3e9ca0965586244e5b02d20521fb77fa1366d588
feliciasanm <47961812+feliciasanm@users.noreply.github.com>
2020-01-20 23:53:24 +0700

    Adding database script to make exploration easier

---
+++ SQL_for_testing.sql
@@ -0,0 +1,11 @@
+-- A script to initialize the form database. Can be run directly or copied to execute.
+
+CREATE DATABASE codeigniter_test;
+USE codeigniter_test;
+
+CREATE TABLE aspirasi (
+  id int(11) NOT NULL,
+  nama varchar(100) NOT NULL,
+  nrp varchar(9) NOT NULL,
+  aspirasi text NOT NULL
+);
