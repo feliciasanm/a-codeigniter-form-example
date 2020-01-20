e68e2bbc375d0cfa2fa7727cc6da61fa1634d176
feliciasanm <47961812+feliciasanm@users.noreply.github.com>
2020-01-20 17:51:34 +0700

    Correcting a small mistake with echo

--- application/views/aspirasi.php
+++ application/views/aspirasi.php
@@ -37,8 +37,8 @@
 			</div>
 		</main>
 			
-		<script src="<?php base_url() . '/assets/js/jquery-3.4.1.slim.min.js'; ?>"></script>
-		<script src="<?php base_url() . '/assets/js/popper.min.js'; ?>"></script>
-		<script src="<?php base_url() . '/assets/js/bootstrap.min.js'; ?>"></script>
+		<script src="<?php echo base_url() . '/assets/js/jquery-3.4.1.slim.min.js'; ?>"></script>
+		<script src="<?php echo base_url() . '/assets/js/popper.min.js'; ?>"></script>
+		<script src="<?php echo base_url() . '/assets/js/bootstrap.min.js'; ?>"></script>
 	</body>
 </html>
